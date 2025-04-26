2d37fbeb3cddbb663b4733444a012f9b9b019ceb
ihalilbesli <ihalilbesli@gmail.com>
2025-04-26 16:02:57 +0300

    css yapılıyor

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -7,49 +7,45 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   showPassword: boolean = false;
-  constructor(
-    private authService:AuthService,
-    private router:Router
-  ){}
+  email: string = '';
+  password: string = '';
 
-      email:string="";
-      password:string="";
+  constructor(private authService: AuthService, private router: Router) {}
 
-      onlogin(){
-      
-      }
-      onSubmit(){
-      this.authService.login(this.email,this.password).subscribe({
-        next:(response:any)=>{
-          const token = response.token; 
-          localStorage.setItem('jwtToken', token);
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
 
-          // Token'ı decode ediyoruz
-          const decoded=this.authService.decodeToken(token)
-          localStorage.setItem('userRole', decoded?.role || "");
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
 
-          if(decoded?.role==="HASTA"){
-           this.router.navigate(['/hasta-dashboard']);  
-           
-          }
-          else if (decoded?.role === 'DOKTOR') {
-           this.router.navigate(['/doktor-dashboard']);
-        
-          } else if (decoded?.role === 'ADMIN') {
-            this.router.navigate(['/admin-dashboard']);
-        
-          }
-        },
-        error:(err)=>{
-          console.error('Giriş başarısız:', err);
-      alert('Giriş başarısız! Lütfen email ve şifrenizi kontrol edin.');
+  onSubmit() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response: any) => {
+        const token = response.token;
+        localStorage.setItem('jwtToken', token);
+        const decoded = this.authService.decodeToken(token);
+        localStorage.setItem('userRole', decoded?.role || "");
+
+        if (decoded?.role === "HASTA") {
+          this.router.navigate(['/hasta-dashboard']);
+        } else if (decoded?.role === 'DOKTOR') {
+          this.router.navigate(['/doktor-dashboard']);
+        } else if (decoded?.role === 'ADMIN') {
+          this.router.navigate(['/admin-dashboard']);
         }
-      })
-    }
+      },
+      error: (err) => {
+        console.error('Giriş başarısız:', err);
+        alert('Giriş başarısız! Lütfen email ve şifrenizi kontrol edin.');
+      }
+    });
+  }
 }
